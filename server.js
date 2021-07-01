c7597d897c026c257ade6f6a5909883993caba8d
Remy Chazelas <remychazelas@gmail.com>
2021-07-01 12:37:57 +0200

    first commit dreamzKatcher <3

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const cookieParser = require('cookie-parser')
+const cookieParser = require('cookie-parser');
 const userRoutes = require('./routes/user.routes');
 const postRoutes = require('./routes/post.routes');
 require('dotenv').config({ path: './config/.env' });
